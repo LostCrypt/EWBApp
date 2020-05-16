936ef89ccaadc70ae8af8f5a366267e97a79f23b
Quanturus <43767802+Quanturus@users.noreply.github.com>
2020-05-16 19:22:18 +1000

    Initial Commit

--- EWBApp/Game1.cs
+++ EWBApp/Game1.cs
@@ -31,10 +31,6 @@ namespace EWBApp
             base.Initialize();
         }
 
-        /// <summary>
-        /// LoadContent will be called once per game and is the place to load
-        /// all of your content.
-        /// </summary>
         protected override void LoadContent()
         {
             // Create a new SpriteBatch, which can be used to draw textures.
