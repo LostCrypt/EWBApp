6de0fbcd07fbdeb61293c0f0ddb3e523d2ed2744
Quanturus <43767802+Quanturus@users.noreply.github.com>
2020-05-16 20:28:47 +1000

    Initial Screen Setup
    Added code to setup the screen. Current resoultion set to 375 x 667 per Apple's resolution for the iPhone 6+ (1080 x 1920).

--- EWBApp/Game1.cs
+++ EWBApp/Game1.cs
@@ -1,58 +1,57 @@
-﻿using Microsoft.Xna.Framework;
+﻿using System;
+using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using System.Collections.Generic;
 
 namespace EWBApp
 {
-    /// <summary>
-    /// This is the main type for your game.
-    /// </summary>
     public class Game1 : Game
     {
+        /// Define Variables here
+        //Graphics
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
 
+        //The screen
+        Rectangle screen;
+
         public Game1()
         {
+            //Viewport
             graphics = new GraphicsDeviceManager(this);
+
+            //Pipeline
             Content.RootDirectory = "Content";
+
+            //Screen Size - 1080 x 1920
+            graphics.PreferredBackBufferWidth = 375;
+            graphics.PreferredBackBufferHeight = 667;
         }
 
-        /// <summary>
-        /// Allows the game to perform any initialization it needs to before starting to run.
-        /// This is where it can query for any required services and load any non-graphic
-        /// related content.  Calling base.Initialize will enumerate through any components
-        /// and initialize them as well.
-        /// </summary>
         protected override void Initialize()
         {
-            // TODO: Add your initialization logic here
+            //SCREEN//
 
-            base.Initialize();
+            //Screen Dimensions
+            screen.Width = GraphicsDevice.Viewport.Width;
+            screen.Height = GraphicsDevice.Viewport.Height;
+
+            //Screen Position
+            screen.X = 0;
+            screen.Y = 0;
         }
 
         protected override void LoadContent()
         {
-            // Create a new SpriteBatch, which can be used to draw textures.
             spriteBatch = new SpriteBatch(GraphicsDevice);
-
-            // TODO: use this.Content to load your game content here
         }
 
-        /// <summary>
-        /// UnloadContent will be called once per game and is the place to unload
-        /// game-specific content.
-        /// </summary>
         protected override void UnloadContent()
         {
             // TODO: Unload any non ContentManager content here
         }
 
-        /// <summary>
-        /// Allows the game to run logic such as updating the world,
-        /// checking for collisions, gathering input, and playing audio.
-        /// </summary>
-        /// <param name="gameTime">Provides a snapshot of timing values.</param>
         protected override void Update(GameTime gameTime)
         {
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
@@ -63,15 +62,13 @@ namespace EWBApp
             base.Update(gameTime);
         }
 
-        /// <summary>
-        /// This is called when the game should draw itself.
-        /// </summary>
-        /// <param name="gameTime">Provides a snapshot of timing values.</param>
         protected override void Draw(GameTime gameTime)
         {
-            GraphicsDevice.Clear(Color.CornflowerBlue);
+            GraphicsDevice.Clear(Color.White);
 
-            // TODO: Add your drawing code here
+            //spriteBatch.Begin();
+
+            //spriteBatch.End();
 
             base.Draw(gameTime);
         }
